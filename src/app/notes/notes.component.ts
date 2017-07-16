435eedf04443436fa7ee20b460239c5b1e53027b
Eduardo <ibredu@gmail.com>
2017-07-16 11:16:02 -0500

    ngIf Básico

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class NotesComponent {
     title = 'Notes Component';
     notasListas = false;
+    action = 'new';
     constructor(){
         setTimeout(()=>{
             this.notasListas = true;
