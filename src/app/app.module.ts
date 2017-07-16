36838916a20587ea93f57fd2948ec9abf77f3919
Eduardo <ibredu@gmail.com>
2017-07-16 11:07:32 -0500

    Two Way Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NotesComponent} from "./notes/notes.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {NotesComponent} from "./notes/notes.component";
       NotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
